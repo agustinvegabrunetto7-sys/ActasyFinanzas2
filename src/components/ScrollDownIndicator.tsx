b5987e8445cbd65f840c6903a9950a8e57778c25
[dyad] <agustinvegabrunetto7@gmail.com>
2025-11-29 22:45:16 -0300

    [dyad] Cambiando el color del indicador de scroll a blanco - wrote 1 file(s)

--- src/components/ScrollDownIndicator.tsx
+++ src/components/ScrollDownIndicator.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ScrollDownIndicator = () => {
   return (
     <div className="absolute bottom-24 left-1/2 -translate-x-1/2 z-20">
-      <p className="text-sky-400 text-lg font-bold neon-glow animate-neon-blink">
+      <p className="text-white text-lg font-bold neon-glow animate-neon-blink">
         Deslizá hacia abajo
       </p>
     </div>

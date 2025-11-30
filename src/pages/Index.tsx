7e1430c819f8cdce3df8366ed29248ad1fbbc1c2
[dyad] <agustinvegabrunetto7@gmail.com>
2025-11-30 00:48:59 -0300

    [dyad] Mover logo principal más arriba - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
           <img
             src="/EUGABLANCO.png"
             alt="Estudiantes Unidos Logo"
-            className="absolute top-[15%] left-1/2 -translate-x-1/2 w-64 h-64 sm:w-40 sm:h-40 md:w-48 md:h-48 drop-shadow-lg" // Aumentado el tamaño base para móvil
+            className="absolute top-[10%] left-1/2 -translate-x-1/2 w-64 h-64 sm:w-40 sm:h-40 md:w-48 md:h-48 drop-shadow-lg" // Movido más arriba (de top-[15%] a top-[10%])
           />
           <h1
             className="text-5xl sm:text-8xl md:text-8xl font-extrabold tracking-tighter drop-shadow-2xl mb-4 text-sky-400"
